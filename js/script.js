cee7221e1abc0b73d2df500102f0a198d2482cda
olgasvitlychna <olga.svitlychna9@gmail.com>
2020-10-16 18:58:23 +0300

    add carousel

--- js/script.js
+++ js/script.js
@@ -1,3 +1,36 @@
+$(document).ready(function() {
+    $('a.myLinkModal').click( function(event){
+        event.preventDefault();
+        $('#myOverlay').fadeIn(297,	function(){
+            $('#myModal')
+                .css('display', 'block')
+                .animate({opacity: 1}, 198);
+        });
+    });
+
+    $('#myModal__close, #myOverlay').click( function(){
+        $('#myModal').animate({opacity: 0}, 198, function(){
+            $(this).css('display', 'none');
+            $('#myOverlay').fadeOut(297);
+        });
+    });
+});
+
+var menuBtn = document.querySelector('.menu-btn');
+var nav = document.querySelector('nav');
+var lineOne = document.querySelector('nav .menu-btn .line--1');
+var lineTwo = document.querySelector('nav .menu-btn .line--2');
+var lineThree = document.querySelector('nav .menu-btn .line--3');
+var link = document.querySelector('nav .nav-links');
+menuBtn.addEventListener('click', () => {
+    nav.classList.toggle('nav-open');
+    lineOne.classList.toggle('line-cross');
+    lineTwo.classList.toggle('line-fade-out');
+    lineThree.classList.toggle('line-cross');
+    link.classList.toggle('fade-in');
+})
+
+
 $(document).ready(function(){
 
     $('.owl-carousel').owlCarousel({
